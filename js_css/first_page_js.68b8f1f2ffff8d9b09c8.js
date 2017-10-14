953eb624038af031ccd4fda3af3e34158de81ff7
JamJonson <JamJonson@163.com>
2017-10-14 15:15:25 +0800

    testewbpack
    testewbpack

---
+++ js_css/first_page_js.68b8f1f2ffff8d9b09c8.js
@@ -0,0 +1,19 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
+
+console.log("check");console.log("check22");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+],[0]);
